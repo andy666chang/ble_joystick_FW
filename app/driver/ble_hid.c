b884431f6711b31d7ae9b73326e3011fcb4dc9f8
andy666chang <andy850821@gmail.com>
2025-12-17 10:40:44 +0800

    fix: advertising after disconnect

--- app/driver/ble_hid.c
+++ app/driver/ble_hid.c
@@ -38,6 +38,28 @@ static const struct bt_data sd[] = {
 	BT_DATA(BT_DATA_NAME_COMPLETE, CONFIG_BT_DEVICE_NAME, sizeof(CONFIG_BT_DEVICE_NAME) - 1),
 };
 
+
+static void adv_work(struct k_work *work)
+{
+	// 強制停止任何可能殘留的廣播任務
+    bt_le_adv_stop();
+
+    // 延遲一小段時間（選填，有時控制器反應較慢）
+    k_sleep(K_MSEC(10)); 
+
+	/* 重點：連線中斷後，手動重啟廣播 */
+	int err = bt_le_adv_start(BT_LE_ADV_CONN_ONE_TIME, ad, ARRAY_SIZE(ad),
+				  sd, ARRAY_SIZE(sd));
+	if (err) {
+		LOG_ERR("Advertising failed to start (err %d)", err);
+	} else {
+		LOG_INF("Advertising successfully restarted");
+	}
+}
+
+static K_WORK_DELAYABLE_DEFINE(adv_work_obj, adv_work);
+
+
 static void connected(struct bt_conn *conn, uint8_t err)
 {
 	char addr[BT_ADDR_LE_STR_LEN];
@@ -63,6 +85,8 @@ static void disconnected(struct bt_conn *conn, uint8_t reason)
 	bt_addr_le_to_str(bt_conn_get_dst(conn), addr, sizeof(addr));
 
 	LOG_INF("Disconnected from %s (reason 0x%02x)", addr, reason);
+
+	k_work_reschedule(&adv_work_obj, K_MSEC(1000));
 }
 
 static void security_changed(struct bt_conn *conn, bt_security_t level,
@@ -144,26 +168,6 @@ void clear_all_bonds(void)
     } else {
         LOG_INF("所有綁定資訊已清除，請重啟裝置或重新廣告\n");
     }
-}
-
-static void re_advertise(void)
-{
-	clear_all_bonds();
-
-	// 強制停止任何可能殘留的廣播任務
-    bt_le_adv_stop();
-
-    // 延遲一小段時間（選填，有時控制器反應較慢）
-    k_sleep(K_MSEC(10)); 
-
-	/* 重點：連線中斷後，手動重啟廣播 */
-	int err = bt_le_adv_start(BT_LE_ADV_CONN_ONE_TIME, ad, ARRAY_SIZE(ad),
-				  sd, ARRAY_SIZE(sd));
-	if (err) {
-		printk("Advertising failed to start (err %d)\n", err);
-	} else {
-		printk("Advertising successfully restarted\n");
-	}
 }
 
 static void ble_hid(void)
